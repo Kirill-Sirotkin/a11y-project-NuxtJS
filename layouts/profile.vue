f6aec6cbc198d960696b3a2403baa0e0b1eaa2c1
Kirill Sirotkin <kiriyms@gmail.com>
2025-05-26 01:14:23 +0300

    fix: add template to profile layout

---
+++ layouts/profile.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="flex flex-col min-h-screen">
+        <LayoutsDefaultHeader />
+        <main class="flex-1">
+            <slot />
+        </main>
+        <LayoutsDefaultFooter />
+    </div>
+</template>
